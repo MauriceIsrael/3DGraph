802a01687b586ccba79348c625f8f89d182e6a97
Momo <maurice.israel@free.fr>
2023-11-21 19:10:58 +0100

    2 spheres et une ligne

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,74 @@
-<script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+<script>
+  import { T, useFrame } from '@threlte/core'
+  import { interactivity, Grid, MeshLineGeometry, MeshLineMaterial, OrbitControls } from '@threlte/extras'
+  import { spring } from 'svelte/motion'
+  import { LineCurve3, Vector3 } from 'three'
+
+  interactivity()
+  const scale = spring(1)
+  let rotation = 0
+  useFrame((state, delta) => {
+    rotation += delta
+  })
+
+
+
+   // create a smooth curve from 4 points
+   const curve = new LineCurve3(new Vector3(4, 4, 0), new Vector3(8, 8, 0))
+  // convert curve to an array of 100 points
+  const points = curve.getPoints(100)
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
-</T.PerspectiveCamera>
-
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
 
 <Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+	infiniteGrid
+	sectionColor="#4a4b4a"
+	sectionSize={20}
+	cellSize={20}
+	fadeDistance={400}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
+<T.PerspectiveCamera makeDefault position={[10, 100, 100]} fov={60}>
+	<OrbitControls enableDamping />
+</T.PerspectiveCamera>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+<T.DirectionalLight position={[0, 10, 10]} castShadow />
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
+<T.Mesh
+  
+  position.y={1}
+  scale={$scale}
+  on:pointerenter={() => scale.set(1.5)}
+  on:pointerleave={() => scale.set(1)}
+  castShadow
 >
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
+  <T.BoxGeometry args={[1, 2, 1]} />
+  <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
+<T.Mesh rotation.x={-Math.PI/2} receiveShadow>
+  <T.CircleGeometry args={[4, 40]}/>
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
+
+<T.Mesh position.x={4} position.y={4}>
+  <T.SphereGeometry />
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
+
+<T.Mesh position.x={8} position.y={8}>
+  <T.SphereGeometry />
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
+
+<T.Mesh
+  position.x={0}
+  position.y={0}
+  scale={1}
 >
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+  <MeshLineGeometry
+    {points}
+    shape="taper"
+  />
+  <MeshLineMaterial color="#fe3d00" />
+</T.Mesh>

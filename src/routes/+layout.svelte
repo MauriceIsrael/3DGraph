3f1fcd444e38e70f590235efda65466464448470
Momo <maurice.israel@free.fr>
2023-11-26 17:50:52 +0100

    minor updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,33 @@
 <script lang="ts">
-    import TopAppBar, {
-      Row,
-      Section,
-      Title,
-      AutoAdjust,
-    } from '@smui/top-app-bar';
-    import IconButton, { Icon } from '@smui/icon-button';
-    import LinearProgress from '@smui/linear-progress';
-  
-    let topAppBar: TopAppBar;
-  </script>
+  import Tab, { Icon, Label } from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+  import LinearProgress from '@smui/linear-progress';
+
+  let tabs = [
+    {
+      icon: 'home',
+      label: 'Home',
+      route: '/',
+    },
+    {
+      label: 'About',
+      route: '/about'
+    }
+  ];
+  let active = tabs[0];
+
+</script>
 
 <LinearProgress indeterminate />
-<TopAppBar bind:this={topAppBar} variant="standard" dense>
-    <nav> 
-        <Row>
-            <Section>
-                <a href="/">
-                  <IconButton class="material-icons">diversity_2</IconButton>
-                </a>
-            </Section>
-                
-            <Section align="end" toolbar>
-                <a href="/about">
-                    <IconButton class="material-icons">question_mark</IconButton>
-                </a>
-            </Section>
-       </Row>
-      </nav>
-</TopAppBar>
-<AutoAdjust {topAppBar}>
-  <slot />
-</AutoAdjust>
+<TabBar {tabs} let:tab bind:active>
+  <nav> 
+    <Tab {tab} href={tab.route}>
+      {#if tab.icon != ''}
+      <Icon class="material-icons">{tab.icon}</Icon>  
+      {/if}
+      <Label>{tab.label}</Label>
+    </Tab>
+  </nav>
+</TabBar>
 
-<style>
-    /* Hide everything above this component. */
-    :global(#smui-app),
-    :global(body),
-    :global(html) {
-      display: block !important;
-      height: auto !important;
-      width: auto !important;
-      position: static !important;
-    }
-  </style>
+<slot />
